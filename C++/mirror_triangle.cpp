2908275423af4bf28b57f5424accab7c5ba1fde9
Sajid M Tariq <sajidmehmoodtariq5@gmail.com>
2024-10-22 05:12:35 -0700

    files added

--- C++/mirror_triangle.cpp
+++ C++/mirror_triangle.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int rows = 4;
 
     for (int i = 0; i < rows; i++) {
         // Print leading spaces
         for (int j = 0; j < i; j++) {
-            std::cout << " ";
+            cout << " ";
         }
         // Print asterisks
         for (int j = 0; j < rows - i; j++) {
-            std::cout << "*";
+            cout << "*";
         }
         // Move to the next line after each row
-        std::cout << std::endl;
+        cout << endl;
     }
 
     return 0;
--- C++/triangle_reverse.cpp
+++ C++/triangle_reverse.cpp
@@ -17,6 +17,7 @@ int main() {
         cout  << endl;
     }
     
+    
     // Exiting the program
     return 0;
 }
